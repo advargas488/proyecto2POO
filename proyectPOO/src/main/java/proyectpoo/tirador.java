32ec0ee8d72e8b27151eb2a320ae4e8dd848838a
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-17 17:35:25 -0600

    Update tirador.java

--- proyectPOO/src/main/java/proyectpoo/tirador.java
+++ proyectPOO/src/main/java/proyectpoo/tirador.java
@@ -2,6 +2,7 @@ package proyectpoo;
 public class tirador extends personaje{
     protected int tiroExtra;
     protected int critico;
+    protected int vampirismo;
     
     public tirador(int nuevoAtaque, int nuevaVida, String nuevaArma,int nuevoRango, int nuevoTiroExtra, int critico, int nuevoRuido){
         super(nuevoAtaque,nuevaVida,nuevaArma,nuevoRango,nuevoRuido);
