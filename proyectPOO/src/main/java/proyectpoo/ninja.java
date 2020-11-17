9b4729ba6b5f1632c1d42a1fc17322b55b7204fc
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-17 17:34:25 -0600

    Update ninja.java

--- proyectPOO/src/main/java/proyectpoo/ninja.java
+++ proyectPOO/src/main/java/proyectpoo/ninja.java
@@ -3,6 +3,7 @@ package proyectpoo;
 public class ninja extends personaje {
     protected int dañoExtra;
     protected int sangrado;
+    protected int danoabsorbido;
     
     public ninja(int nuevoAtaque, int nuevaVida, String nuevaArma,int nuevoRango, int nuevoDañoExtra, int nuevoSangrado, int nuevoRuido){
         super(nuevoAtaque,nuevaVida,nuevaArma,nuevoRango,nuevoRuido);
