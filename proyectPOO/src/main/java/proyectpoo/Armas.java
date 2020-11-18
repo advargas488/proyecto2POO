9c199d0003f1e039148be7f658229bfa1c5ab6df
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-18 17:32:13 -0600

    Update Armas.java

--- proyectPOO/src/main/java/proyectpoo/Armas.java
+++ proyectPOO/src/main/java/proyectpoo/Armas.java
@@ -1,16 +1,20 @@
 package proyectpoo;
 
 public class Armas {
+    // atributos
     private String nombre;
     protected int ataque;
     protected int nivelRuido;
     protected int rango;
+    // constructor
     public Armas (String _nombre, int _ataque, int _nivelruido, int _rango){
         this.nombre = _nombre;
         this.ataque = _ataque;
         this.nivelRuido = _nivelruido;
         this.rango = _rango;
     }
+   
+    // getters de los atributos del arma
     public String getNombre(){
         return nombre;
     }
