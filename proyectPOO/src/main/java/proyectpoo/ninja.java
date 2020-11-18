a8de50056afd37e269054ea4fab77d087827cc9e
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-18 17:39:03 -0600

    Update ninja.java

--- proyectPOO/src/main/java/proyectpoo/ninja.java
+++ proyectPOO/src/main/java/proyectpoo/ninja.java
@@ -1,16 +1,18 @@
 package proyectpoo;
 
+// Clase extendida de la clase base personaje,aplicando herencia
 public class ninja extends personaje {
     protected int dañoExtra;
     protected int sangrado;
     protected int danoabsorbido;
-
+  // Constructor de la subclase ninja con el metodo super para agregarle los atributos de la clase base personaje
     public ninja(int nuevoAtaque, int nuevaVida, String nuevaArma,int nuevoRango, int nuevoDañoExtra, int nuevoSangrado, int nuevoRuido,int danoabsorbido,int nuevoNivel){
         super(nuevoAtaque,nuevaVida,nuevaArma,nuevoRango,nuevoRuido,nuevoNivel);
         this.dañoExtra = nuevoDañoExtra;
         this.sangrado = nuevoSangrado;
         this.danoabsorbido = danoabsorbido;
     }
+    // getters para los atributos de esta instancia 
     public int getHabilidad1(){
         return dañoExtra;
     }
