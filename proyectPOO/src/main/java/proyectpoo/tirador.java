5ded8f3e7dd0e6f28372b8d650681b3f2a28c082
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-18 17:49:12 -0600

    Update tirador.java

--- proyectPOO/src/main/java/proyectpoo/tirador.java
+++ proyectPOO/src/main/java/proyectpoo/tirador.java
@@ -1,16 +1,18 @@
 package proyectpoo;
+// Clase extendida de la clase base personaje,aplicando herencia
 public class tirador extends personaje{
+    // Atributos de la subclase con un acesso protected para accederlos dentro del paquete
     protected int tiroExtra;
     protected int critico;
     protected int vampirismo;
-
+    // Constructor de la subclase tirador con el metodo super para agregarle los atributos de la clase base
     public tirador(int nuevoAtaque, int nuevaVida, String nuevaArma,int nuevoRango, int nuevoTiroExtra, int critico, int nuevoRuido, int vampirismo, int nuevoNivel){
         super(nuevoAtaque,nuevaVida,nuevaArma,nuevoRango,nuevoRuido,nuevoNivel);
         this.tiroExtra = nuevoTiroExtra;
         this.critico = critico;
         this.vampirismo = vampirismo;
     }
-
+    // getters para los atributos de esta instancia 
     public int getHabilidad1(){
         return tiroExtra;
     }
