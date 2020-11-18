cb8b722d9784727566ea46f05b56cf3d2d72ef4f
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-18 17:53:38 -0600

    Update Main.java

--- proyectPOO/src/main/java/proyectpoo/Main.java
+++ proyectPOO/src/main/java/proyectpoo/Main.java
@@ -1,7 +1,8 @@
 package proyectpoo;
-
+// Clase main 
 public class Main {
     public static void main(String[] args){
+        // Creacion del tablero y llamada de la ventana de la interfaz
         tablero zombieDefense = new tablero();
         zombieDefense.setVisible(true);
     }
