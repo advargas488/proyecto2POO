023a84caf03506822c3aa3e2005aa26cc1f6e7bc
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-18 17:40:18 -0600

    Update items.java

--- proyectPOO/src/main/java/proyectpoo/items.java
+++ proyectPOO/src/main/java/proyectpoo/items.java
@@ -1,13 +1,16 @@
 package proyectpoo;
 
+// atributos de los items que dropean los zombies
 public class items {
     private int Vida;
     private int dano;
-
+    
+    // constructor de la clase 
     public items (int _vida, int _dano){
         this.Vida = _vida;
         this.dano = _dano;
     }
+    // getters de los atributos de la clase
     public int getVida(){
         return Vida;
     }
