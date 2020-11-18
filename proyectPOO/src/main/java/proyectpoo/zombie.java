8bde97329553862e13ba7c05ee7a549250e1ecf6
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-18 17:46:33 -0600

    Update zombie.java

--- proyectPOO/src/main/java/proyectpoo/zombie.java
+++ proyectPOO/src/main/java/proyectpoo/zombie.java
@@ -1,14 +1,17 @@
 package proyectpoo;
+// clase base del zombie 
 public class zombie {
+    // atributos del zombie con una acceso protected para el acesso a estos dentro del mismo paquete
     protected int vida;
     protected int dano;
     protected String item;
-    
+    // constructor del zombie base
     public zombie(int _vida, int _dano, String _item) {
         this.vida = _vida;
         this.dano = _dano;
         this.item = _item;
     }
+    // getters de los atributos de la clase base
     public int getVida(){
         return vida;
     }
