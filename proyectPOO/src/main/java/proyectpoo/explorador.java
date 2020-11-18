0f220320a00c01b153e7991a8aa0f8089ad5fb65
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-11-18 17:34:40 -0600

    Update explorador.java

--- proyectPOO/src/main/java/proyectpoo/explorador.java
+++ proyectPOO/src/main/java/proyectpoo/explorador.java
@@ -1,16 +1,19 @@
 package proyectpoo;
 
+// Clase extendida de la clase base personaje,aplicando herencia
 public class explorador extends personaje {
+    // atributos
     protected int escudoProtector;
     protected int regeneracionVida;
-
     protected int vidaextra;
+    // Constructor y el super para declarar los atributos de la clase base
     public explorador(int nuevoAtaque, int nuevaVida, String nuevaArma,int nuevoRango, int escudo, int nuevaRegeneracionVida, int nuevoRuido, int nuevaVidaExtra, int nuevoNivel){
         super(nuevoAtaque,nuevaVida,nuevaArma,nuevoRango,nuevoRuido,nuevoNivel);
         this.escudoProtector = escudo;
         this.regeneracionVida = nuevaRegeneracionVida;
         this.vidaextra = nuevaVidaExtra;
     }
+    // getters de los atributos del objeto
     public int getHabilidad1(){
         return escudoProtector;
     }
